0883446b37dd7b5a64219751d74b3eb916d4f343
Valentin Lerouge <cloudd2vi@gmail.com>
2025-06-25 13:24:29 +0200

    Mise à jour de l'URL de base pour le client d'authentification

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL || "http://37.59.115.240/",
   plugins: [adminClient()],
 });
 
